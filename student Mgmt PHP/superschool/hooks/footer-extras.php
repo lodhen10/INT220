ac5aeb5a7d9b16017e1c10d76374bf455128ab6f
tenzin11 <lodhen10zin@gmail.com>
2022-04-10 22:44:06 +0530

    my project

---
+++ student Mgmt PHP/superschool/hooks/footer-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong>Student Management &nbsp;<a href="https://code-projects.org/">Brought To You By code-projects.org</a>.</strong>
+  </footer>
